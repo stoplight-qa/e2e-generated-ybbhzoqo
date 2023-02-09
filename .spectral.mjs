b9ea16dba062f3f56cfbfbb68ca56ff3ec4a2295
qa-sejpx-2-smkw@example.com <qa-sejpx-2-smkw@example.com>
2023-02-09 15:28:20 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15204.stoplight-dev.com/api/v1/projects/cHJqOjE3Mw/spectral.js?branch_slug=main&token=25fd2a1e-21ec-46ca-b85f-57852a82157e";
+export default { extends: ruleset };
